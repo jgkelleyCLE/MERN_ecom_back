b8d152afbfa160f5b4248fb4dc94e8d185c72d59
Jack <74841896+jgkelleyCLE@users.noreply.github.com>
2024-09-23 14:40:43 -0400

    schematics

--- models/productModel.js
+++ models/productModel.js
@@ -37,6 +37,10 @@ const ProductSchema = new mongoose.Schema({
         type: String,
         required: false
     },
+    schematic: {
+        type: String,
+        required: false
+    },
     parts: [
         {
             _id: {
